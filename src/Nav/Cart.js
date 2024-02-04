679af43d37545d1663905882b5f6a7cc40825ffc
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-04 11:51:17 +0530

    cart

--- src/Nav/Cart.js
+++ src/Nav/Cart.js
@@ -1,12 +1,31 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux'
-// const Cart = () => {
+ import React from 'react'
+ import { useSelector } from 'react-redux'
+ import CartItem from './CartItem';
+
+const Cart = () => {
 // const cartItems=useSelector(state=>state.ecom.cart)
 // //  console.log(Data);
 
-import { useSelector } from "react-redux";
 
-
+ const cart=useSelector((state)=>state.cart)
+return(
+<div className="cart__left">
+  <div>
+    <h3>Shopping Cart</h3>
+    {cart?.map((item) => (
+      <CartItem
+        key={item.id}
+        id={item.id}
+        image={item.Image}
+        title={item.Device}
+        quantity={item.quantity}
+      />
+    ))}
+  </div>
+</div>
+)
+    }
+    export default Cart
 //   return (
 // <div className='CartWrapper'>
 // <hr/>
@@ -29,20 +48,3 @@ import { useSelector } from "react-redux";
 
 
 
-export const cart=useSelector((state)=>state.cart)
-return(
-<div className="cart__left">
-  <div>
-    <h3>Shopping Cart</h3>
-    {cart?.map((item) => (
-      <CartItem
-        key={item.id}
-        id={item.id}
-        image={item.Image}
-        title={item.Device}
-        quantity={item.quantity}
-      />
-    ))}
-  </div>
-</div>
-)
