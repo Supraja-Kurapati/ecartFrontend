071c33dd869382a56356047c200435af4d29d1d8
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-04 11:46:31 +0530

    cart

--- src/Nav/Cart.js
+++ src/Nav/Cart.js
@@ -29,7 +29,6 @@ import { useSelector } from "react-redux";
 
 
 
-import { useSelector } from "react-redux";
 export const cart=useSelector((state)=>state.cart)
 return(
 <div className="cart__left">
